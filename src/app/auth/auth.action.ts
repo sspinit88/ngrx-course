f2e5d2466f106ac0b2781014784fda721c5d78fd
Sergey Pashnin <sspinit88@gmail.com>
2019-11-13 22:39:56 +0300

    готов auth.action (login)

---
+++ src/app/auth/auth.action.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from '@ngrx/store';
+import { User } from './model/user.model';
+
+export const login = createAction(
+  // [Login Page] - ограничиваем страницу входа, вторым парметром - событие или команда, соответствующая действию
+  // props ринимает полезную нагрузку, указываем тип этой нагрузки (payload)
+  '[Login Page] User Login',
+  props<{user: User}>(),
+);
