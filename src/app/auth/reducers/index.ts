debc511d99710b0cc40c9a3ef381a5521ea9c8c6
Sergey Pashnin <sspinit88@gmail.com>
2019-11-13 23:13:37 +0300

    reducer start

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,15 +1,26 @@
 import {
   ActionReducer,
   ActionReducerMap,
-  createFeatureSelector,
+  createFeatureSelector, createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer, on
 } from '@ngrx/store';
+import { AuthActions } from '../auth.action';
 
-export interface State {
 
+export interface AuthState {
+  user: undefined;
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const initialState: AuthState = {};
 
-};
+// export const reducers: ActionReducerMap<AuthState> = {};
+
+export const authReducer = createReducer(
+  initialState, // объект первой инициализации
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    };
+  })
+);
