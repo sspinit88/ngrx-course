926523205e4eb42e40a3a11b4a9ab75f636dea86
Sergey Pashnin <sspinit88@gmail.com>
2019-11-14 18:19:22 +0300

    Meta Reducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -15,5 +15,17 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer,
 };
 
+// метаредьюсер принимает редьюсер, который небходимо вызвать
+// NgRx Meta Reducers это функции, вызываемые сразу после возникновения действия, но перед тем, как они будут переданы глобальному редюсеру.
+// На практике они чаще всего используются для логирования.
+export function logged(reducer: ActionReducer<any>): ActionReducer<any> {
+  return ((state, action) => {
+    console.log('File: index.ts, Class: logged, Line: 21, state():', state);
+    console.log('File: index.ts, Class: logged, Line: 22, action():', action);
+    return reducer(state, action); // возвращаем результат, передаем текущее состояние и переданный экшн
+  });
+}
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] =
+  // MetaReducer будут выполняться по порядку
+  !environment.production ? [logged] : [];
