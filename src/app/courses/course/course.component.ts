79aaecdcd6d9e5225f66a2f1581d0330e8892261
Sergey Pashnin <sspinit88@gmail.com>
2019-11-15 23:39:59 +0300

    course.component бага есть, add changeDetection: ChangeDetectionStrategy.OnPush

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
 import { Observable } from 'rxjs';
@@ -11,7 +11,8 @@ import { LessonEntityService } from '../services/lesson-entity.service';
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
 
