7278617718956a6d0fc909197239c6e89b7cadc8
Sergey Pashnin <sspinit88@gmail.com>
2019-11-15 22:23:19 +0300

    удаление карточки

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,49 +1,56 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {EditCourseDialogComponent} from "../edit-course-dialog/edit-course-dialog.component";
-import {defaultDialogConfig} from '../shared/default-dialog-config';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Course } from '../model/course';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
+import { defaultDialogConfig } from '../shared/default-dialog-config';
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
 })
 export class CoursesCardListComponent implements OnInit {
 
-    @Input()
-    courses: Course[];
+  @Input()
+  courses: Course[];
 
-    @Output()
-    courseChanged = new EventEmitter();
+  @Output()
+  courseChanged = new EventEmitter();
 
-    constructor(
-      private dialog: MatDialog ) {
-    }
+  constructor(
+    private dialog: MatDialog,
+    private courseService: CourseEntityService,
+  ) {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
 
-    editCourse(course:Course) {
+  editCourse(course: Course) {
 
-        const dialogConfig = defaultDialogConfig();
+    const dialogConfig = defaultDialogConfig();
 
-        dialogConfig.data = {
-          dialogTitle:"Edit Course",
-          course,
-          mode: 'update'
-        };
+    dialogConfig.data = {
+      dialogTitle: 'Edit Course',
+      course,
+      mode: 'update'
+    };
 
-        this.dialog.open(EditCourseDialogComponent, dialogConfig)
-          .afterClosed()
-          .subscribe(() => this.courseChanged.emit());
+    this.dialog.open(EditCourseDialogComponent, dialogConfig)
+      .afterClosed()
+      .subscribe(() => this.courseChanged.emit());
 
-    }
+  }
 
-  onDeleteCourse(course:Course) {
+  onDeleteCourse(course: Course) {
 
-
+    this.courseService.delete(course)
+      .subscribe(
+        () => console.log('File: courses-card-list.component.ts, Line: 51, `Delete is completed!`():', `Delete is completed!`),
+        err => console.log('File: courses-card-list.component.ts, Line: 52, err():', err),
+      );
   }
 
 }
